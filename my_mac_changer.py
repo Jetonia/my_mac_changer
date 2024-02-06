b3f0fa8d76e809036090d6c5187bcbe0d8c4f010
Jetonia <41581461+Jetonia@users.noreply.github.com>
2024-02-06 21:19:10 +0300

    Add files via upload

---
+++ my_mac_changer.py
@@ -0,0 +1,16 @@
+import subprocess
+import optparse
+
+parse_object = optparse.OptionParser()
+parse_object.add_option("-i","--interface" , dest="interface" , help="interface to change!")
+parse_object.add_option("-m" , "--mac", dest="mac_adress" , help="new mac adress")
+
+(user_inputs,arguments) = parse_object.parse_args()
+user_interface = user_inputs.interface
+user_mac_adress = user_inputs.mac_adress
+
+print("MyMacCHanger started!")
+
+subprocess.call(["ifconfig",user_interface, "down"])
+subprocess.call(["ifconfig" , user_interface, "hw" , "ether" , user_mac_adress])
+subprocess.call(["ifconfig",user_interface,"up"])
