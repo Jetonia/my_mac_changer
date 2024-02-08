9c32ac67a875798a895c3b995bf42f3d18b922a2
Mert <b.mertbay@gmail.com>
2024-02-08 03:07:51 +0300

    I created functions to make it more organized and also when the mac adress changes will be able to see it directly.

--- my_mac_changer.py
+++ my_mac_changer.py
@@ -1,16 +1,24 @@
 import subprocess
 import optparse
 
-parse_object = optparse.OptionParser()
-parse_object.add_option("-i","--interface" , dest="interface" , help="interface to change!")
-parse_object.add_option("-m" , "--mac", dest="mac_adress" , help="new mac adress")
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i","--interface" , dest="interface" , help="interface to change!")
+    parse_object.add_option("-m" , "--mac", dest="mac_adress" , help="new mac adress")
 
-(user_inputs,arguments) = parse_object.parse_args()
-user_interface = user_inputs.interface
-user_mac_adress = user_inputs.mac_adress
+    return parse_object.parse_args()
+
+def change_mac_adress(user_interface, user_mac_adress):
+    subprocess.call(["ifconfig",user_interface , "down"])
+    subprocess.call(["ifconfig" , user_interface , "hw" , "ether" , user_mac_adress])
+    subprocess.call(["ifconfig",user_interface , "up"])
+
+def control_new_mac(interface):
+
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+    print(ifconfig)
 
 print("MyMacCHanger started!")
-
-subprocess.call(["ifconfig",user_interface, "down"])
-subprocess.call(["ifconfig" , user_interface, "hw" , "ether" , user_mac_adress])
-subprocess.call(["ifconfig",user_interface,"up"])
+(user_input,arguments) = get_user_input()
+change_mac_adress(user_input.interface,user_input.mac_adress)
+control_new_mac(user_input.interface)
